dd2cffe230fefd54be4fe01cb3600cb0b7bab03e
Patrick Jones <pjones@numedics.com>
2017-09-21 08:03:41 -0700

    moved connection string to configuration

--- NuDataDb/EF/NuMedicsGlobalContext.cs
+++ NuDataDb/EF/NuMedicsGlobalContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace NuDataDb.EF
 {
@@ -80,11 +81,21 @@ namespace NuDataDb.EF
         public virtual DbSet<Users> Users { get; set; }
         public virtual DbSet<UserTypes> UserTypes { get; set; }
 
+        public IConfiguration Configuration { get; }
+
         // can only have a single option per instance.
         private bool UseDefaultBuilder { get; set; } = true;
 
-        public NuMedicsGlobalContext(DbContextOptions<NuMedicsGlobalContext> options) : base(options) { UseDefaultBuilder = false; }
-        public NuMedicsGlobalContext() { }
+        public NuMedicsGlobalContext(IConfiguration configuration, DbContextOptions<NuMedicsGlobalContext> options) : base(options)
+        {
+            Configuration = configuration;
+            UseDefaultBuilder = false;
+        }
+
+        public NuMedicsGlobalContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -92,7 +103,8 @@ namespace NuDataDb.EF
             if (UseDefaultBuilder)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=STAGESERVER\SQLDEV2014;Database=NuMedicsGlobal;User=nuweb;Password=P@ssw0rd;Trusted_Connection=True;MultipleActiveResultSets=true");
+                //optionsBuilder.UseSqlServer(@"Server=STAGESERVER\SQLDEV2014;Database=NuMedicsGlobal;User=nuweb;Password=P@ssw0rd;Trusted_Connection=True;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("NumedicsGlobal"));
             }
         }
 
