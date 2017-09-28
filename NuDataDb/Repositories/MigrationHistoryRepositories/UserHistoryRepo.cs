7c2dd3b2f1f9a259763a03ed75c767b1e3e858c5
Patrick Jones <pjones@numedics.com>
2017-09-28 13:32:44 -0700

    Added MigrationHistoryRepo

---
+++ NuDataDb/Repositories/MigrationHistoryRepositories/UserHistoryRepo.cs
@@ -0,0 +1,44 @@
+﻿using NuDataDb.EFMigrationHistory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuDataDb.Repositories.MigrationHistoryRepositories
+{
+    public class UserHistoryRepo : BaseRepo<UserHistory, MigrationHistoryContext>
+    {
+        public UserHistoryRepo(MigrationHistoryContext dbContext) : base(dbContext)
+        {
+        }
+
+        public override UserHistory GetSingle(int id)
+        {
+            try
+            {
+                return ctx.UserHistory.FirstOrDefault(f => f.MigrationId == id);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error getting {typeof(UserHistory)} entity from database. /n/r Entity Id: {id}", e);
+            }
+        }
+
+        public override void Delete(int id)
+        {
+            try
+            {
+                var del = ctx.UserHistory.FirstOrDefault(f => f.MigrationId == id);
+                if (del != null)
+                {
+                    ctx.Remove(del);
+                    Save();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error deleting {typeof(UserHistory)} entity from database. /n/r Entity Id: {id}", e);
+            }
+        }
+    }
+}
