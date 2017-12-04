22a19e5026ad2709d9f632cb63e2809f2fb38bf8
Patrick Jones <pjones@numedics.com>
2017-12-04 15:11:54 -0800

    Added SleepPatternLevelsRepo

---
+++ NuDataDb/Repositories/SleepPattenLevelsRepo.cs
@@ -0,0 +1,44 @@
+﻿using NuDataDb.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuDataDb.Repositories
+{
+    public class SleepPattenLevelsRepo : BaseRepo<SleepPatternLevels, NuMedicsGlobalContext>
+    {
+        public SleepPattenLevelsRepo(NuMedicsGlobalContext dbContext) : base(dbContext)
+        {
+        }
+
+        public override SleepPatternLevels GetSingle(int id)
+        {
+            try
+            {
+                return ctx.SleepPatternLevels.FirstOrDefault(f => f.PatternId == id);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error getting {typeof(SleepPatternLevels)} entity from database. /n/r Entity Id: {id}", e); ;
+            }
+        }
+
+        public override void Delete(int id)
+        {
+            try
+            {
+                var del = ctx.SleepPatternLevels.FirstOrDefault(f => f.PatternId == id);
+                if (del != null)
+                {
+                    ctx.Remove(del);
+                    Save();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error deleting {typeof(SleepPatternLevels)} entity from database. /n/r Entity Id: {id}", e); ;
+            }
+        }
+    }
+}
