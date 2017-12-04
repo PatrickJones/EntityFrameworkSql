276ee7909219c0d1f61a10d2b32a434c58b5f079
Patrick Jones <pjones@numedics.com>
2017-12-04 15:16:37 -0800

    Added SleepDataRepo

---
+++ NuDataDb/Repositories/SleepDataRepo.cs
@@ -0,0 +1,45 @@
+﻿using NuDataDb.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuDataDb.Repositories
+{
+    public class SleepDataRepo : BaseRepo<SleepData, NuMedicsGlobalContext>
+    {
+        public SleepDataRepo(NuMedicsGlobalContext dbContext) : base(dbContext)
+        {
+        }
+
+        public override SleepData GetSingle(int id)
+        {
+            try
+            {
+                return ctx.SleepData.FirstOrDefault(f => f.SleepDataId == id);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error getting {typeof(SleepData)} entity from database. /n/r Entity Id: {id}", e); ;
+            }
+        }
+
+        public override void Delete(int id)
+        {
+            try
+            {
+                var del = ctx.SleepData.FirstOrDefault(f => f.SleepDataId == id);
+                if (del != null)
+                {
+                    ctx.Remove(del);
+                    Save();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error deleting {typeof(SleepData)} entity from database. /n/r Entity Id: {id}", e); ;
+            }
+        }
+
+    }
+}
