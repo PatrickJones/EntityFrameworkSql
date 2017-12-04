6d56f51b6cd98d3055fe94c61bd8b5bb51026b04
Patrick Jones <pjones@numedics.com>
2017-12-04 14:46:21 -0800

    Added ActivityTypesRepo

---
+++ NuDataDb/Repositories/ActivityTypesRepo.cs
@@ -0,0 +1,44 @@
+﻿using NuDataDb.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NuDataDb.Repositories
+{
+    public class ActivityTypesRepo : BaseRepo<ActivityTypes, NuMedicsGlobalContext>
+    {
+        public ActivityTypesRepo(NuMedicsGlobalContext dbContext) : base(dbContext)
+        {
+        }
+
+        public override ActivityTypes GetSingle(int id)
+        {
+            try
+            {
+                return ctx.ActivityTypes.FirstOrDefault(f => f.ActivityId == id);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error getting {typeof(ActivityTypes)} entity from database. /n/r Entity Id: {id}", e);
+            }
+        }
+
+        public override void Delete(int id)
+        {
+            try
+            {
+                var del = ctx.ActivityTypes.FirstOrDefault(f => f.ActivityId == id);
+                if (del != null)
+                {
+                    ctx.Remove(del);
+                    Save();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error deleting {typeof(ActivityTypes)} entity from database. /n/r Entity Id: {id}", e);
+            }
+        }
+    }
+}
